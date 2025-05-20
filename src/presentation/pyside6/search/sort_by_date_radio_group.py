6047dcf85d420e650e9badb981ce0944704ac01b
S-Del <s-del.pg@protonmail.com>
2025-05-20 12:19:31 +0900

    [add] 抜けていた SearchConditionBuilder のテスト

---
+++ src/presentation/pyside6/search/sort_by_date_radio_group.py
@@ -0,0 +1,51 @@
+from PySide6.QtWidgets import (
+    QButtonGroup,
+    QHBoxLayout,
+    QLabel,
+    QVBoxLayout,
+    QWidget
+)
+from typing import Literal
+
+from presentation.pyside6.shared import RadioButtonWithStrValue
+
+
+VALID_RADIO_VALUE = Literal["DESC", "ASC"]
+
+
+class SortByDateRadioGroup(QWidget):
+    def __init__(self):
+        super().__init__()
+        radio_group_label = QLabel("検索結果の並び順を指定")
+
+        radio_group = QWidget()
+        desc_radio = RadioButtonWithStrValue[VALID_RADIO_VALUE](
+            "新しい順", "DESC"
+        )
+        desc_radio.setChecked(True)
+        asc_radio = RadioButtonWithStrValue[VALID_RADIO_VALUE](
+            "古い順", "ASC"
+        )
+        self.radio_group = QButtonGroup()
+        self.radio_group.addButton(desc_radio)
+        self.radio_group.addButton(asc_radio)
+        radio_layout = QHBoxLayout()
+        radio_layout.addWidget(desc_radio)
+        radio_layout.addWidget(asc_radio)
+        radio_group.setLayout(radio_layout)
+
+        layout = QVBoxLayout()
+        layout.addWidget(radio_group_label)
+        layout.addWidget(radio_group)
+
+        self.setLayout(layout)
+
+    @property
+    def order_by(self) -> VALID_RADIO_VALUE:
+        checked = self.radio_group.checkedButton()
+        if not isinstance(checked, RadioButtonWithStrValue):
+            raise TypeError("ラジオボタンの型が不正")
+        return checked.value
+
+    def reset(self):
+        self.radio_group.buttons()[0].setChecked(True)
---
+++ tests/infrastructure/sqlite/result/test_search_condition_builder.py
@@ -0,0 +1,87 @@
+from datetime import date
+from typing import Any
+from pytest import mark, raises
+
+from domain.model.result import FirstOrSecond, ResultChar
+from domain.repository.result import FetchResultQuery
+from domain.shared.unit import NonEmptyStr
+from infrastructure.sqlite.config.table import ResultTableConfig
+from infrastructure.sqlite.result import SearchType, SearchConditionBuilder
+
+
+@mark.parametrize(
+    ("value", "search_type", "expected_value"),
+    [
+        ("\\", "exact", "\\\\"),
+        ("%", "exact","\\%"),
+        ("_", "exact", "\\_"),
+        ("a", "exact", "a"),
+        ("b", "partial", "%b%"),
+        ("c", "prefix", "c%"),
+        ("d", "suffix", "%d")
+    ]
+)
+def test_escape_like_param(
+    value: str,
+    search_type: SearchType,
+    expected_value: str
+):
+    escaped = SearchConditionBuilder.escape_like_param(value, search_type)
+    assert escaped == expected_value
+
+
+@mark.parametrize(
+    "search_type",
+    ["invalid_search_type"]
+)
+def test_excape_like_param_invalid_search_type(search_type):
+    with raises(ValueError):
+        SearchConditionBuilder.escape_like_param("", search_type)
+
+
+@mark.parametrize(
+    ("query", "expected_where", "expected_params"),
+    [(
+        {"first_or_second": [FirstOrSecond('F'), FirstOrSecond('S')]},
+        f" WHERE {ResultTableConfig.COLUMN_NAMES.FIRST_OR_SECOND} IN (?, ?)",
+        ['F', 'S']
+    ), (
+        {"result": [ResultChar('W'), ResultChar('L'), ResultChar('D')]},
+        f" WHERE {ResultTableConfig.COLUMN_NAMES.RESULT} IN (?, ?, ?)",
+        ['W', 'L', 'D']
+    ), (
+        {"my_deck_name": NonEmptyStr("MY_DECK_NAME")},
+        f" WHERE {ResultTableConfig.COLUMN_NAMES.MY_DECK_NAME}"
+        " LIKE ? ESCAPE '\\'",
+        ["MY\\_DECK\\_NAME"]
+    ), (
+        {"opponent_deck_name": NonEmptyStr("OPPONENT_DECK_NAME")},
+        f" WHERE {ResultTableConfig.COLUMN_NAMES.OPPONENT_DECK_NAME}"
+        " LIKE ? ESCAPE '\\'",
+        ["OPPONENT\\_DECK\\_NAME"]
+    ), (
+        {"since": date.fromisoformat("2025-05-20")},
+        f" WHERE {ResultTableConfig.COLUMN_NAMES.REGISTER_DATE} >= ?",
+        ["2025-05-20T00:00:00"]
+    ), (
+        {"until": date.fromisoformat("2025-05-20")},
+        f" WHERE {ResultTableConfig.COLUMN_NAMES.REGISTER_DATE} <= ?",
+        ["2025-05-20T23:59:59"]
+    ), (
+        {
+            "since": date.fromisoformat("2025-05-20"),
+            "until": date.fromisoformat("2025-05-20")
+        },
+        f" WHERE {ResultTableConfig.COLUMN_NAMES.REGISTER_DATE} >= ?"
+        f" AND {ResultTableConfig.COLUMN_NAMES.REGISTER_DATE} <= ?",
+        ["2025-05-20T00:00:00", "2025-05-20T23:59:59"]
+    )]
+)
+def test_search_condition_builder(
+    query: FetchResultQuery,
+    expected_where: str,
+    expected_params: list[Any]
+):
+    where_clause, params = SearchConditionBuilder().build(query)
+    assert where_clause == expected_where
+    assert params == expected_params
